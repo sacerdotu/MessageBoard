0427c62da300251cff1bc065a3700c5dce25af48
Andrei Tenea <andrei.tenea93@gmail.com>
2017-01-03 15:45:36 +0200

    MessageBoard design modifications

--- src/MessageBoardController/Properties/AssemblyInfo.cs
+++ src/MessageBoardController/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("57a67442-efc8-4839-b225-830c729c8c60")]
+[assembly: Guid("6fabf54b-7dc5-4e18-99a0-e0cddd7a09f2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
